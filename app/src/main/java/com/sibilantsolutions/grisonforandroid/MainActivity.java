c522a36cab9541f7dcbffa21b4504409bf3222bd
Joel Geiger <joel@sibilantsolutions.com>
2016-12-07 22:02:42 -0500

    Start work to delete a camdef upon long press.

--- app/src/main/java/com/sibilantsolutions/grisonforandroid/MainActivity.java
+++ app/src/main/java/com/sibilantsolutions/grisonforandroid/MainActivity.java
@@ -12,12 +12,16 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.ActionMode.Callback;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -64,6 +68,7 @@ public class MainActivity extends ListActivity {
     private SharedPreferences sharedPreferences;
 
     private MyCamArrayAdapter myCamArrayAdapter;
+    private ActionMode mActionMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,7 +94,70 @@ public class MainActivity extends ListActivity {
         myCamArrayAdapter = new MyCamArrayAdapter(this, camSessions);
 
         setListAdapter(myCamArrayAdapter);
+
+        getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mActionMode != null) {
+                    return false;
+                }
+
+                // Start the CAB
+                mActionMode = startActionMode(actionModeCallback(position));
+                view.setSelected(true);
+                return true;
+            }
+        });
     }
+
+    private Callback actionModeCallback(final int position) {
+        return new Callback() {
+
+            // Called when the action mode is created; startActionMode() was called
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                // Inflate a menu resource providing context menu items
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.context_menu, menu);
+                return true;
+            }
+
+            // Called each time the action mode is shown. Always called after onCreateActionMode, but
+            // may be called multiple times if the mode is invalidated.
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false; // Return false if nothing is done
+            }
+
+            // Called when the user selects a contextual menu item
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete_cam:
+                        deleteItem(position);
+                        mode.finish(); // Action picked, so close the CAB
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            // Called when the user exits the action mode
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                mActionMode = null;
+            }
+
+        };
+    }
+
+    private void deleteItem(final int position) {
+        final CamSession camSession = myCamArrayAdapter.getItem(position);
+        //TODO: camSession.stop();
+        //TODO: Remove from preferences.
+        myCamArrayAdapter.remove(camSession);
+    }
+
 
     public void onClickAddCam(View view) {
         startActivityForResult(new Intent(this, AddCamActivity.class), REQ_ADD_CAM);
