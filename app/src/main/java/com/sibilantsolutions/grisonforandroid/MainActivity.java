3a73085cc0d28712908471b721fd69ff51a5631c
Joel Geiger <joel@sibilantsolutions.com>
2017-07-15 22:35:31 -0400

    Start camera list activity.

--- app/src/main/java/com/sibilantsolutions/grisonforandroid/MainActivity.java
+++ app/src/main/java/com/sibilantsolutions/grisonforandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sibilantsolutions.grisonforandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, CamListActivity.class));
     }
 }
